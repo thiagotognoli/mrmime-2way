b8e1a1c7f05d6705b511cdbd207fd5fedb29de91
Thiago Tognoli <thiago@clockin.it>
2024-04-20 16:08:38 -0300

    charset sugestion from https://github.com/lukeed/mrmime/issues/2

--- src/$mimeToExtension.ts
+++ src/$mimeToExtension.ts
@@ -3,3 +3,19 @@ const mimes = {};
 function mimeToExtensionLookup(mime) {
 	return mimes[("" + mime).trim().toLowerCase()];
 }
+
+const EXTRACT_TYPE_REGEXP = /^\s*([^;\s]*)(?:;|\s|$)/;
+const TEXT_TYPE_REGEXP = /^text\//i;
+
+// export function mimeCharset(type) {
+// 	if (!type || typeof type !== "string") return false;
+// 	const match = EXTRACT_TYPE_REGEXP.exec(type);
+// 	const mime = match && db[match[1].toLowerCase()];
+
+// 	if (mime && mime.charset) return mime.charset;
+
+// 	// default text/* to utf-8
+// 	if (match && TEXT_TYPE_REGEXP.test(match[1])) return "UTF-8";
+
+// 	return false;
+// }
